34bdfb2fc4d6020f26f1d5a61661bdf6720e05a7
AlcosaC <alcosa@floridauniversitaria.es>
2023-01-24 12:56:47 +0100

    calcu

---
+++ test/CalculadoraTest.php
@@ -0,0 +1,17 @@
+<?php
+use PHPUnit\Framework\TestCase;
+include 'Calculadora.php';
+final class CalculadoraTest extends TestCase
+{
+public function testSuma()
+{
+$calc = new Calculadora(3,4);
+$this->assertEquals(7, $calc->suma());
+}
+public function testResta()
+{
+$calc = new Calculadora(3,4);
+$this->assertEquals(-1, $calc->resta());
+}
+}
+?>
